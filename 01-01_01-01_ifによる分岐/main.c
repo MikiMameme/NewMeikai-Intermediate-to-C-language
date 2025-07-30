e95d4519609ac73d80ebe202ae74da0f9dc0af1b
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-07-30 17:22:50 +0900

    01-01_01-01_ifによる分岐
    数当てゲーム（その１：試作版）

---
+++ 01-01_01-01_ifによる分岐/main.c
@@ -0,0 +1,25 @@
+/* 数当てゲーム（その１：試作版） */
+
+#include<stdio.h>
+
+int main(void)
+{
+	int no;
+	int ans = 7;
+	
+	printf("０～９の整数を当ててください。\n\n");
+	
+	printf("いくつかな: ");
+	scanf("%d", &no);
+	
+	if(no > ans)
+		printf("\aもっと小さいよ。\n");
+	
+	else if(no < ans)
+		printf("\aもっと大きいよ。\n");
+	
+	else
+		printf("正解です。\n");
+	
+	return 0;
+}
