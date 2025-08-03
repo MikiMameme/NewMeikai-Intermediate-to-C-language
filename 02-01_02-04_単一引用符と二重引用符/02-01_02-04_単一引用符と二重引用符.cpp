9199f94738635b29076f50dddbfa6f0cdaf6f119
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-03 20:13:01 +0900

    02-01_02-04_単一引用符と二重引用符
    拡張表記'と"の利用例

---
+++ 02-01_02-04_単一引用符と二重引用符/02-01_02-04_単一引用符と二重引用符.cpp
@@ -0,0 +1,20 @@
+﻿/* 拡張表記'と"の利用例 */
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+int main(void)
+{
+	printf("文字列リテラルと文字定数について\n");
+
+	printf("二重引用符");
+	putchar('"');
+	printf("で囲まれた\"ABC\"は文字列リテラルです。\n");
+
+	printf("単一引用符");
+	putchar('\'');
+	printf("で囲まれた'A'は文字定数です。\n");
+
+	return 0;
+}
