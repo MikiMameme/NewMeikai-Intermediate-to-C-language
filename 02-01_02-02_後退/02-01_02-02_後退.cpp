371639c75a612d9ea655fa3d566e7a81728bb81d
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-03 17:18:28 +0900

    02-01_02-02_後退
    後退\bの利用例:１秒毎ごとに１文字ずつ消去

---
+++ 02-01_02-02_後退/02-01_02-02_後退.cpp
@@ -0,0 +1,34 @@
+﻿/* 後退\bの利用例:１秒毎ごとに１文字ずつ消去 */
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<time.h>
+
+/* xミリ秒経過するのを待つ */
+int sleep(unsigned long x)
+{
+	clock_t c1 = clock(), c2;
+
+	do {
+		if ((c2 = clock()) == (clock_t)-1)
+			return 0;
+	} while (1000.0 * (c2 - c1) / CLOCKS_PER_SEC < x);
+
+	return 1;
+}
+
+int main(void)
+{
+	int i;
+
+	printf("ABCDEFG");
+
+	for (i = 0; i < 7; i++) {
+		sleep(1000);
+		printf("\b \b");
+		fflush(stdout);
+	}
+
+	return 0;
+}
