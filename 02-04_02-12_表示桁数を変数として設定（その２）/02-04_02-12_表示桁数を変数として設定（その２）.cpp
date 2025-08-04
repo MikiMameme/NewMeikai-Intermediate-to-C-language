889ffb0a295bc1e0e3d5fcc087ee4d0a031b999a
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-04 19:14:31 +0900

    02-04_02-12_表示桁数を変数として設定（その２）
    数字文字を1桁ずつずらしながら表示（その２）

---
+++ 02-04_02-12_表示桁数を変数として設定（その２）/02-04_02-12_表示桁数を変数として設定（その２）.cpp
@@ -0,0 +1,20 @@
+﻿/*数字文字を1桁ずつずらしながら表示（その２）*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+int main(void)
+{
+	int i;
+	int x;
+
+	printf("何段表示しますか :");
+	scanf("%d", &x);
+
+	for (i = 1; i <= x; i++) {
+		printf("%*d\n", i, i % 10);
+	}
+
+	return 0;
+}
