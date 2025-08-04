d32a6d674320e5e6100ccfaeb97f96f80fa9b099
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-04 18:03:35 +0900

    02-03_02-09_テロップ表示（左方向）
    名前をテロップ表示（その１：左方向へ文字を流す）

---
+++ 02-03_02-09_テロップ表示（左方向）/02-03_02-09_テロップ表示（左方向）.cpp
@@ -0,0 +1,47 @@
+﻿/*名前をテロップ表示（その１：左方向へ文字を流す）*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<string.h>
+#include<time.h>
+
+/*xミリ秒経過するのを待つ*/
+int sleep(unsigned long x)
+{
+	clock_t c1 = clock(), c2;
+
+	do {
+		if ((c2 = clock()) == (clock_t)-1)
+			return 0;
+	} while (1000.0 * (c2 - c1) / CLOCKS_PER_SEC < x);
+	return 1;
+}
+
+int main(void)
+{
+	int i;
+	int cnt = 0;
+	char name[] = "Miki Mame ";
+	int name_len = strlen(name);
+
+	while (1) {
+		putchar('\r');
+
+		for (i = 0; i < name_len; i++) {
+			if (cnt + i < name_len)
+				putchar(name[cnt + i]);
+			else
+				putchar(name[cnt + i - name_len]);
+		}
+
+		fflush(stdout);
+		sleep(500);
+
+		if (cnt < name_len - 1)
+			cnt++;
+		else
+			cnt = 0;
+	}
+	return 0;
+}
