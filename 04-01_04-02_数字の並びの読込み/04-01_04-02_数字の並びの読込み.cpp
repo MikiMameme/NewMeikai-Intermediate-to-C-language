47358b8d51afa949151e48f95018e5b8ea13fb46
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-08 16:11:53 +0900

    04-01_04-02_数字の並びの読込み
    整数値の読込みと表示

---
+++ 04-01_04-02_数字の並びの読込み/04-01_04-02_数字の並びの読込み.cpp
@@ -0,0 +1,17 @@
+﻿/*整数値の読込みと表示*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+int main(void)
+{
+	int x;
+
+	printf("整数を入力してください:");
+	scanf("%d", &x);
+
+	printf("%dと入力しましたね。\n", x);
+
+	return 0;
+}
