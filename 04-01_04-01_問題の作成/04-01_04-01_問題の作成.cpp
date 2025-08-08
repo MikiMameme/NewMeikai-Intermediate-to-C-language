7175d4659ab131179b8b4d42a17fb638045a4725
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-08 16:06:34 +0900

    04-01_04-01_問題の作成
    相異なる4つの数字の並びを生成して配列xに格納(コードのみ)

---
+++ 04-01_04-01_問題の作成/04-01_04-01_問題の作成.cpp
@@ -0,0 +1,15 @@
+﻿/*相異なる4つの数字の並びを生成して配列xに格納(コードのみ)*/
+
+void make4digits(int x[])
+{
+	int i, j, val;
+
+	for (i = 0; i < 4; i++) {
+		do {
+			val = rand() % 10;
+			for (j = 0; j < i; i++)
+				if (val == x[j])
+					break;
+		} while (j < i);
+	}
+}
