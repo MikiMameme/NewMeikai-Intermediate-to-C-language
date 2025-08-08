9b5611900dba32b94e347122ec86fb89a37e27a5
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-08 17:22:18 +0900

    04-01_04-C2_関数と引数としてのポインタ2
    和と差を求める関数

---
+++ 04-01_04-C2_関数と引数としてのポインタ2/04-01_04-C2_関数と引数としてのポインタ2.cpp
@@ -0,0 +1,18 @@
+﻿/*和と差を求める関数*/
+
+#include<stdio.h>
+
+void wa_sa(int x, int y, int* wa, int* sa)
+{
+	*wa = x + y;
+	*sa = x - y;
+}
+
+int main(void)
+{
+	int a = 5, b = 3, p = 1, m = 1;
+	wa_sa(a, b, &p, &m);
+	printf("p = %d m = %d\n", p, m);
+
+	return 0;
+}
