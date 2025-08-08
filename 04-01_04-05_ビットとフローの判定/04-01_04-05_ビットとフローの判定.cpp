c681ecfafb094c5407fc263acb07c801bea236cb
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-08 16:55:36 +0900

    04-01_04-05_ビットとフローの判定
    ビットとフローの判定(関数のみ)

---
+++ 04-01_04-05_ビットとフローの判定/04-01_04-05_ビットとフローの判定.cpp
@@ -0,0 +1,16 @@
+﻿/*ビットとフローの判定(関数のみ)*/
+void judge(const char s[], int no[], int* hit, int* blow)
+{
+	int i, j;
+
+	*hit = *blow = 0;
+	for (i = 0; i < 4; i++) {
+		for(j = 0; j < 4; j++)
+		if (s[i] == '0' + no[j])
+			if (i == j)
+				(*hit)++;
+			else
+				(*blow)++;
+		}
+	}
+}
