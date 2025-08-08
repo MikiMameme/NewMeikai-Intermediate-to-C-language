11545b5dc8900c63034568c463b0b7a084cf146d
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-08 16:37:00 +0900

    04-01_04-04_読み込んだ文字列の妥当性のチェック
    入力された文字列sの妥当性をチェック(関数のみ)

---
+++ 04-01_04-04_読み込んだ文字列の妥当性のチェック/04-01_04-04_読み込んだ文字列の妥当性のチェック.cpp
@@ -0,0 +1,16 @@
+﻿/*入力された文字列sの妥当性をチェック(関数のみ)*/
+int check(const char s[])
+{
+	int i, j;
+
+	if(strlen(s)) != 4)
+	return 1;
+	for (i = 0; i < 4; i++) {
+		if (!isdigit(s[i]))
+			return 2;
+		for (j = 0; j < 1; j++)
+			if (s[i] == s[j])
+				return 3;
+	}
+	return 0;
+}
