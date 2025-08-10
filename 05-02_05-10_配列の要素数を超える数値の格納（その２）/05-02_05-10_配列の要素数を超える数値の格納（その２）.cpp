aa739885e0a547680b1f53c50e8ba7ee973d8c7d
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-10 18:32:44 +0900

    05-02_05-10_配列の要素数を超える数値の格納（その２）
    好きな個数だけ値を読み込んで要素数10の配列に最後の10個を格納（その２）

---
+++ 05-02_05-10_配列の要素数を超える数値の格納（その２）/05-02_05-10_配列の要素数を超える数値の格納（その２）.cpp
@@ -0,0 +1,33 @@
+﻿/*好きな個数だけ値を読み込んで要素数10の配列に最後の10個を格納（その２）*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+#define MAX 10
+
+int main(void)
+{
+	int i;
+	int a[MAX];
+	int cnt = 0;
+	int retry;
+
+	printf("整数を入力してください。\n");
+
+	do {
+		printf("%d個目の整数 :", cnt + 1);
+		scanf("%d", &a[cnt++ % MAX]);
+
+		printf("続けますか？（Yes…1 / No…0）:");
+		scanf("%d", &retry);
+	} while (retry == 1);
+
+	i = cnt - MAX;
+	if (i < 0) i = 0;
+
+	for (; i < cnt; i++)
+		printf("%2d個目 : %d\n", i + 1, a[i % MAX]);
+
+	return 0;
+}
