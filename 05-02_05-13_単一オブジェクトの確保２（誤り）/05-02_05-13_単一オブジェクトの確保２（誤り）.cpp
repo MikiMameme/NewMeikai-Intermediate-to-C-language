77ec0661312c9b7e20e4532e4be40c6772958530
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-10 19:53:46 +0900

    05-02_05-13_単一オブジェクトの確保２（誤り）
    動的に確保した整数にキーボードから読み込んだ値を格納（誤り）

---
+++ 05-02_05-13_単一オブジェクトの確保２（誤り）/05-02_05-13_単一オブジェクトの確保２（誤り）.cpp
@@ -0,0 +1,25 @@
+﻿/*動的に確保した整数にキーボードから読み込んだ値を格納（誤り）*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	int* x;
+
+	x = (int*)calloc(1, sizeof(int));
+
+	if (x == NULL)
+		puts("記憶域の確保に失敗しました。");
+
+	else {
+		printf("*xに格納する値 :");
+		scanf("%d", &x);
+		printf("*x = %d\n", *x);
+		free(x);
+	}
+
+	return 0;
+}
