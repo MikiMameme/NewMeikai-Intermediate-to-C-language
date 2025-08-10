c4c0c2e3dd4c62c8575b11279313358f0fd8e836
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-10 17:00:29 +0900

    05-02_05-08_配列への数値の格納
    最大10個の値を読み込んで要素数10の配列に格納

---
+++ 05-02_05-08_配列への数値の格納/05-02_05-08_配列への数値の格納.cpp
@@ -0,0 +1,34 @@
+﻿/*最大10個の値を読み込んで要素数10の配列に格納*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+#define MAX 10
+
+int main(void)
+{
+	int i;
+	int a[MAX];
+	int cnt = 0;
+	int retry;
+
+	printf("整数を入力してください。\n");
+	printf("入力できるのは最大で%d個です。\n", MAX);
+
+	do {
+		printf("%d個目の整数:", cnt + 1);
+		scanf("%d", &a[cnt++]);
+
+		if (cnt == MAX)
+			break;
+
+		printf("続けますか？(Yes…1 / No…0):");
+		scanf("%d", &retry);
+	} while (retry == 1);
+
+	for (i = 0; i < cnt; i++)
+		printf("%2d個目 : %d\n", i + 1, a[i]);
+
+	return 0;
+}
