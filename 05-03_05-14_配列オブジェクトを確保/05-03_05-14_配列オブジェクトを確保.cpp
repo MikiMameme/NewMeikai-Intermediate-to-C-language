99d0be35d338445f1407ab3df2b55e1f9480480e
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-10 20:04:09 +0900

    05-03_05-14_配列オブジェクトを確保
    整数配列を動的に確保

---
+++ 05-03_05-14_配列オブジェクトを確保/05-03_05-14_配列オブジェクトを確保.cpp
@@ -0,0 +1,33 @@
+﻿/*整数配列を動的に確保*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	int* x;
+	int n;
+
+	printf("確保する配列の要素 :");
+	scanf("%d", &n);
+
+	x = (int*)calloc(n, sizeof(int));
+
+	if (x == NULL)
+		puts("記憶域の確保に失敗しました。");
+	else {
+		int i;
+
+		for (i = 0; i < n; i++)
+			x[i] = i;
+
+		for (i = 0; i < n; i++)
+			printf("x[%d] = %d\n", i, x[i]);
+
+		free(x);
+	}
+
+	return 0;
+}
