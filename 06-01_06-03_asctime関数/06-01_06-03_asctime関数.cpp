838e3ad23ffe33865e89f5a552d0e1de46a9831d
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-11 12:45:49 +0900

    06-01_06-03_asctime関数
    現在の日付・時刻を表示（その２：asctime関数を利用）

---
+++ 06-01_06-03_asctime関数/06-01_06-03_asctime関数.cpp
@@ -0,0 +1,15 @@
+﻿/*現在の日付・時刻を表示（その２：asctime関数を利用）*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<time.h>
+
+int main(void)
+{
+	time_t current = time(NULL);
+
+	printf("現在の日付・時刻 :%s", asctime(localtime(&current)));
+
+	return 0;
+}
