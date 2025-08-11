635bf6aa917b6337bdecb440fd6d6bd82f82069c
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-11 13:27:59 +0900

    06-01_06-07_difftime関数
    暗算能力チェック（０～９９の整数を４つ加算するのに要する時間を計測）

---
+++ 06-01_06-07_difftime関数/06-01_06-07_difftime関数.cpp
@@ -0,0 +1,38 @@
+﻿/*暗算能力チェック（０～９９の整数を４つ加算するのに要する時間を計測）*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(void)
+{
+	int a, b, c, d;
+	int x;
+	time_t start, end;
+
+	srand(time(NULL));
+
+	a = rand() % 100;
+	b = rand() % 100;
+	c = rand() % 100;
+	d = rand() % 100;
+
+	printf("%d + %d + %d + %dは何ですか :", a, b, c, d);
+
+	start = time(NULL);
+
+	while (1) {
+		scanf("%d", &x);
+		if (x == a + b + c + d)
+			break;
+		printf("\a違います、再入力してください :");
+	}
+
+	end = time(NULL);
+
+	printf("%.01f秒かかりました。\n", difftime(end, start));
+
+	return 0;
+}
