273689d3c32075c6fd81b3ecf077e72d0fd3a54b
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-13 21:28:56 +0900

    06-04_06-15_ポインタによるargvの文字単位の走査
    プログラム名・プログラム仮引数の表示

---
+++ 06-04_06-15_ポインタによるargvの文字単位の走査/06-04_06-15_ポインタによるargvの文字単位の走査.cpp
@@ -0,0 +1,19 @@
+﻿/*プログラム名・プログラム仮引数の表示*/
+
+#include<stdio.h>
+
+int main(int argc, char** argv)
+{
+	int i = 0;
+	char c;
+
+	while (argc-- > 0) {
+		printf("argv[%d] = \"", i++);
+		while (c = *(*argv)++)
+			putchar(c);
+		argv++;
+		printf("\"\n");
+	}
+
+	return 0;
+}
