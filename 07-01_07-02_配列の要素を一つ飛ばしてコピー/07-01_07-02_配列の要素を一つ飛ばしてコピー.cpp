2f292341f7a584643d29ed36db73d21abbeac67d
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-15 16:08:31 +0900

    07-01_07-02_配列の要素を一つ飛ばしてコピー
    配列をコピーして表示（要素を一つ飛ばす）

---
+++ 07-01_07-02_配列の要素を一つ飛ばしてコピー/07-01_07-02_配列の要素を一つ飛ばしてコピー.cpp
@@ -0,0 +1,30 @@
+﻿/*配列をコピーして表示（要素を一つ飛ばす）*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(void)
+{
+	int i, j, x;
+	int dgt[9] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	int a[8] = {0};
+
+	srand(time(NULL));
+
+	x = rand() % 9;
+
+	i = j = 0;
+	while (i < 9) {
+		if (i != x)
+			a[j++] = dgt[i];
+		i++;
+	}
+
+	for (i = 0; i < 8; i++)
+		printf("%d", a[i]);
+
+	putchar('\n');
+
+	return 0;
+}
