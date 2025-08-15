6194bd52efddf8d3f0e295e17d44cc6ae3720a3e
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-15 19:02:53 +0900

    07-02_07-06_キー入力と操作性の向上
    getchの活用例、 Visual C++などのMS-DOS / Windowsで動作

---
+++ 07-02_07-06_キー入力と操作性の向上/07-02_07-06_キー入力と操作性の向上.cpp
@@ -0,0 +1,31 @@
+﻿/*getchの活用例
+ Visual C++などのMS-DOS / Windowsで動作*/
+
+#define _CRT_NONSTDC_NO_DEPRECATE
+
+#include<stdio.h>
+#include<conio.h>
+#include<ctype.h>
+
+int main(void)
+{
+	int ch;
+	int retry;
+
+	do {
+		printf("キーを押してください。");
+		ch = getch();
+
+		printf("\n押されたキーは%cで値は%dです。\n", 
+											isprint(ch) ? ch : ' ', ch);
+
+		printf("もう一度？(Y/N) :");
+		retry = getch();
+		if (isprint(retry))
+		putch(retry);
+
+		putchar('\n');
+	} while (retry == 'Y' || retry == 'y');
+
+	return 0;
+}
