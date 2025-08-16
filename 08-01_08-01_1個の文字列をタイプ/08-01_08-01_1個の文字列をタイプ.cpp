e6c7dd865e7646fcff84128d435731c7d2ee77d7
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-16 18:57:43 +0900

    08-01_08-01_1個の文字列をタイプ
    一つの文字列によるキータイピング練習（その１）

---
+++ 08-01_08-01_1個の文字列をタイプ/08-01_08-01_1個の文字列をタイプ.cpp
@@ -0,0 +1,46 @@
+﻿/*一つの文字列によるキータイピング練習（その１）*/
+
+#define _CRT_NONSTDC_NO_DEPRECATE
+
+#include<time.h>
+#include<ctype.h>
+#include<stdio.h>
+#include<string.h>
+#include "getputch.h"
+
+int main(void)
+{
+	int i;
+	const char* str = "How do you do?";
+	int len = strlen(str);
+	clock_t start, end;
+
+	init_getputch();
+
+	printf("このとおりタイプしてください。\n");
+	printf("%s\n", str);
+	fflush(stdout);
+
+	start = clock();
+
+	for (i = 0; i < len; i++) {
+		int ch;
+
+		do {
+			ch = getch();
+			if (isprint(ch)) {
+				putch(ch);
+				if (ch != str[i])
+					putch('\b');
+			}
+		} while (ch != str[i]);
+	}
+
+	end = clock();
+
+	printf("\n%.1f秒でした。\n", (double)(end - start) / CLOCKS_PER_SEC);
+
+	term_getputch();
+
+	return 0;
+}
