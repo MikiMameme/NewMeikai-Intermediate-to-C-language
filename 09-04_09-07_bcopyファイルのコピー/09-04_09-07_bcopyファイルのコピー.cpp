23afeeb6bd856f1b0c5e57a4e61b9f897b1313af
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-23 00:04:31 +0900

    09-04_09-07_bcopyファイルのコピー
    bcopyファイルのコピー（引数なしで動作確認）

---
+++ 09-04_09-07_bcopyファイルのコピー/09-04_09-07_bcopyファイルのコピー.cpp
@@ -0,0 +1,38 @@
+﻿/*bcopy...ファイルのコピー*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+/*この大きさに分割してコピー*/
+#define BSIZE 1024
+
+int main(int argc, char *argv[])
+{
+	int n;
+	FILE  *src, *dst;
+	unsigned char buf[BSIZE];
+
+	if (argc != 3) {
+		fprintf(stderr, "コマンドライン引数がないか、パラメータが不正です\n");
+		fprintf(stderr, "bcopy コピー元ファイル名 コピー先ファイル名\n", *argv);
+	}
+	else {
+		if ((src = fopen(*++argv, "rb")) == NULL) {
+			fprintf(stderr, "ファイル%sがオープンできませんでした\n", *argv);
+				return 1;
+		}
+		else if ((dst = fopen(*++argv, "wb")) == NULL) {
+			fprintf(stderr, "ファイル%sがオープンできませんでした\n", *argv);
+			fclose(src);
+			return 1;
+		}
+		else {
+			while ((n = fread(buf, BSIZE, 1, src)) > 0)
+				fwrite(buf, n, 1, dst);
+			fclose(src);
+			fclose(dst);
+		}
+	}
+	return 0;
+}
