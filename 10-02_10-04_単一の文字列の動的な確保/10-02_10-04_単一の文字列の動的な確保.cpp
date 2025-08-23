caf4af99e8221d666fb654cd964bd06c6268b2af
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-23 15:22:53 +0900

    10-02_10-04_単一の文字列の動的な確保
    文字列を動的に確保

---
+++ 10-02_10-04_単一の文字列の動的な確保/10-02_10-04_単一の文字列の動的な確保.cpp
@@ -0,0 +1,31 @@
+﻿/*文字列を動的に確保*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(void)
+{
+	char st[16];
+	char *pt;
+
+	printf("文字列stを入力してください :");
+	scanf("%s", st);
+
+	/*記憶域を動的に確保*/
+	/*C++のコンパイラのため明示的なキャストが必要*/
+	pt = (char *) malloc(strlen(st) + 1);
+
+	if (pt) {
+		strcpy(pt, st);
+		printf("その文字列の複製ptを作りました\n");
+		printf("st = %s\n", st);
+		printf("pt = %s\n", pt);
+		/*記憶域を開放*/
+		free(pt);
+	}
+
+	return 0;
+}
