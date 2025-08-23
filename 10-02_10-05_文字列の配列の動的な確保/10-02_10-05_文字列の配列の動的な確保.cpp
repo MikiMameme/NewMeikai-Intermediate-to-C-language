336903d18b46ed097ab747665efdacc286dcfe49
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-08-23 15:39:31 +0900

    10-02_10-05_文字列の配列の動的な確保
    文字列の配列を動的に確保

---
+++ 10-02_10-05_文字列の配列の動的な確保/10-02_10-05_文字列の配列の動的な確保.cpp
@@ -0,0 +1,32 @@
+﻿/*文字列の配列を動的に確保*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	int num;
+	char (*p)[15];
+
+	printf("文字列は何個ですか :");
+	scanf("%d", &num);
+
+	p = (char(*)[15])malloc(num * 15);
+	if (p == NULL)
+		puts("記憶域の確保に失敗しました");
+	else {
+		int i;
+		for (i = 0; i < num; i++) {
+			printf("p[%d] :", i);
+			scanf("%s", p[i]);
+		}
+		for (i = 0; i < num; i++)
+			printf("p[%d] = %s\n", i, p[i]);
+
+		free(p);
+	}
+
+	return 0;
+}
